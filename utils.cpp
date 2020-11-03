6e237b8d35ccb176ef184239f238827ea53a2ce6
yimi97 <msif1900@gmail.com>
2020-11-03 14:47:50 -0500

    read file

---
+++ utils.cpp
@@ -0,0 +1,49 @@
+#include <map>
+#include <sstream>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "vectormap.hpp"
+using namespace std;
+
+void readFile(string & file, vector<vector<int>> & landArray, int N){
+    ifstream ifs;
+    ifs.open(file, ifstream::in);
+    string line;
+
+    while(getline(ifs, line)){
+        vector<int> lineParsed;
+        istringstream ss(line);
+        string num;	
+        while (getline(ss, num,' ')){
+            lineParsed.push_back(atoi(num.c_str()));
+        }
+        landArray.push_back(lineParsed);
+    }
+
+    ifs.close();
+}
+
+int main(int argc, char ** argv){
+    int P = atoi(argv[1]);
+    int M = atoi(argv[2]);
+    int A = atoi(argv[3]);
+    int N = atoi(argv[4]);
+    string file = argv[5];
+    vector<vector<int>> landArray;
+    readFile(file, landArray, N);
+
+    // test VectorMap
+    VectorMap * vm = new VectorMap(N);
+    vm->setData(landArray, N);
+    vector<vector<int>> res = vm->getData(0,2);
+    for (size_t i=0;i<res.size();i++){
+        cout << res[i][0]<<" ";
+        cout << res[i][1]<<endl;
+    }
+    delete vm;
+
+    return EXIT_SUCCESS;
+}
